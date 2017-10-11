513144c03da12bf869abd1f3422322eaa9fc1e2b
Kamran <kamran2727@gmail.com>
2017-10-11 14:01:13 +0330

    fix an 'if statement' and edit keyboard bot

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -16,8 +16,9 @@ def user_information(update):
     username_get = update['message']['chat']['username']
     first_name_get = update['message']['chat']['first_name']
     last_name_get = update['message']['chat']['last_name']
-    requests.post( 'http://159.203.69.159:8000/setUserID/',json={"user_id": user_id_get, "username": username_get,
-                                                      "first_name": first_name_get, "last_name": last_name_get},  )
+    requests.post('http://159.203.69.159:8000/setUserID/', json={"user_id": user_id_get, "username": username_get,
+                                                                 "first_name": first_name_get,
+                                                                 "last_name": last_name_get}, )
 
 
 @csrf_exempt
@@ -31,12 +32,13 @@ def message_handler(bot, update):
     chat_id = update.message.chat_id
     message = update.message.text
     print("i am at the top of message handler")
-    if message == HAFEZ_FALL or HAFEZ_FALL_AGAIN:
+    print (message)
+    if message == HAFEZ_FALL or message == HAFEZ_FALL_AGAIN:
         data_get = json.loads(requests.get('http://159.203.69.159:8000/hafezFall/').text)
         print("we are here")
         number = random.randint(1, (int(len(data_get))))
         print ("len of data is", len(data_get))
-        print( "number is ", number)
+        print("number is ", number)
         for data in data_get:
             data_id = data.get('id', '')
             if int(data_id) == int(number):
@@ -44,29 +46,36 @@ def message_handler(bot, update):
                 data_description = data.get('description', '')
                 data_list = "{0}\n\n{1}".format(data_text, data_description)
                 keyboard = [[HAFEZ_FALL_AGAIN]]
-                bot.send_message(chat_id, data_list, reply_markup=ReplyKeyboardMarkup(keyboard))
+                bot.send_message(chat_id, data_list, reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
                 print ("fall has sent")
                 break
         print("the data's in data_get were invalid")
     else:
         print ("incorrect term")
         bot.send_message(chat_id, "عبارت وارد شده ناصحیح است، لطفا دکمه صحیح را انتخاب نمایید.")
+        bot.send_photo(chat_id=chat_id, photo=open('/home/kamran/Desktop/season/pic_for_season.jpg', 'rb'))
 
     print()
     print('\n')
 
+
+def message_handler2(bot, update):
+    pass
+
+
 def start(bot, update):
     chat_id = update.message.chat_id
     user_information(update)
-    keyboard = [[HAFEZ_FALL]]
     print("i am in start")
     keyboard = [[HAFEZ_FALL]]
-    bot.send_message(chat_id, "روز حافظ مبارک باد.", reply_markup=ReplyKeyboardMarkup(keyboard))
+    bot.send_message(chat_id, "روز حافظ مبارک باد.", reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
 
 
 def main():
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(MessageHandler([Filters.text], message_handler))
+    updater.dispatcher.add_handler(MessageHandler([Filters.text], message_handler2))
+
     updater.start_polling()
 
 
